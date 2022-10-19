c3dcfbb623e23ddd0971405dd29463db73571da3
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-10-19 00:09:23 -0300

    NOvas alterações e acertos

--- src/components/Plains/Plains.jsx
+++ src/components/Plains/Plains.jsx
@@ -13,10 +13,15 @@ export function Plains() {
                 </div>
                 <div className="text">
                     <p><IoCheckbox /> Anúncios ilimitados</p>
+                    <p><IoCheckbox /> Agendamento de visitas</p>
+                    <p><IoCheckbox /> Chat único por anúncio</p>
+                    <p><IoCheckbox /> Contato via ligação e whatsapp</p>
                     <p><IoCheckbox /> Área administrativa</p>
-                    <p><IoCheckbox /> App administrativo [Breve]</p>
-                    <p><IoCheckbox /> Anúncios no site Sua Chave</p>
-                    <p><IoCheckbox /> Anúncios no app Sua Chave [Breve]</p>
+                    <p><IoCheckbox /> Envio e recebimento de documentos</p>
+                    <p><IoCheckbox /> Controle de recebimento de aluguéis</p>
+                    <p><IoCheckbox /> App administrativo</p>
+                    <p><IoCheckbox /> Anúncios no site Sua Chave Auto</p>
+                    <p><IoCheckbox /> Anúncios no app Sua Chave Auto</p>
                     <p><IoCheckbox /> Plano sem fidelidade</p>
                 </div>
                 <a href="/plano">Seu plano atual</a>
@@ -28,34 +33,48 @@ export function Plains() {
                 <h4>/ mês</h4>
                 </div>
                 <div className="text">
-                    <p><IoCheckbox /> Anúncios ilimitados</p>
+                <p><IoCheckbox /> Anúncios ilimitados</p>
+                    <p><IoCheckbox /> Agendamento de visitas</p>
+                    <p><IoCheckbox /> Chat único por anúncio</p>
+                    <p><IoCheckbox /> Contato via ligação e whatsapp</p>
+                    <p><IoCheckbox /> Site totalmente responsivo</p>
                     <p><IoCheckbox /> Área administrativa</p>
-                    <p><IoCheckbox /> App administrativo [Breve]</p>
+                    <p><IoCheckbox /> Envio e recebimento de documentos</p>
+                    <p><IoCheckbox /> Controle de recebimento de aluguéis</p>
+                    <p><IoCheckbox /> App administrativo</p>
                     <p><IoCheckbox /> Site individual para sua empresa</p>
-                    <p><IoCheckbox /> Anúncios em seu site</p>
-                    <p><IoCheckbox /> Anúncios no site Sua Chave</p>
-                    <p><IoCheckbox /> Anúncios no app Sua Chave [Breve]</p>
+                    <p><IoCheckbox /> Site individual totalmente responsivo</p>
+                    <p><IoCheckbox /> Anúncios no seu site</p>
+                    <p><IoCheckbox /> Anúncios no site Sua Chave Auto</p>
+                    <p><IoCheckbox /> Anúncios no app Sua Chave Auto</p>
                     <p><IoCheckbox /> Plano sem fidelidade</p>
                 </div>
                 <a href="/plano">Selecionar plano</a>
                 <p>Mais procurado</p>
             </div>
             <div className="plain3">
-                <h3>Plano WebApp [Breve]</h3>
+                <h3>Plano WebApp</h3>
                 <div className="title">
                 <h1>R$ 399,99</h1>
                 <h4>/ mês</h4>
                 </div>
                 <div className="text">
                     <p><IoCheckbox /> Anúncios ilimitados</p>
+                    <p><IoCheckbox /> Agendamento de visitas</p>
+                    <p><IoCheckbox /> Chat único por anúncio</p>
+                    <p><IoCheckbox /> Contato via ligação e whatsapp</p>
+                    <p><IoCheckbox /> Site totalmente responsivo</p>
                     <p><IoCheckbox /> Área administrativa</p>
-                    <p><IoCheckbox /> App administrativo [Breve]</p>
+                    <p><IoCheckbox /> Envio e recebimento de documentos</p>
+                    <p><IoCheckbox /> Controle de recebimento de aluguéis</p>
+                    <p><IoCheckbox /> App administrativo</p>
                     <p><IoCheckbox /> Site individual para sua empresa</p>
-                    <p><IoCheckbox /> App individual para sua empresa [Breve]</p>
+                    <p><IoCheckbox /> Site individual totalmente responsivo</p>
+                    <p><IoCheckbox /> App individual para sua empresa</p>
                     <p><IoCheckbox /> Anúncios no seu site</p>
-                    <p><IoCheckbox /> Anúncios no seu app [Breve]</p>
-                    <p><IoCheckbox /> Anúncios no site Sua Chave</p>
-                    <p><IoCheckbox /> Anúncios no app Sua Chave [Breve]</p>
+                    <p><IoCheckbox /> Anúncios no seu app</p>
+                    <p><IoCheckbox /> Anúncios no site Sua Chave Auto</p>
+                    <p><IoCheckbox /> Anúncios no app Sua Chave Auto</p>
                     <p><IoCheckbox /> Plano sem fidelidade</p>
                 </div>
                 <a href="/plano">Selecionar plano</a>
