96a41a388338fb0a761f959ee8661cda4e6b244d
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-07-13 13:34:42 -0300

    Rotas e Filtro

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'https://api-suachave-auto.herokuapp.com/'
+baseURL: 'https://api-suachave-auto.herokuapp.com/'
 // baseURL: 'http://localhost:3333/'
 });
 
